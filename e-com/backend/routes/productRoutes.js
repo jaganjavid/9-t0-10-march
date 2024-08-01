fc42f29a4f74839ac5a0f1eca8f230b600924e55
jaganjavid <jagantutor1@gmail.com>
2024-08-01 21:46:24 +0530

    Added a test

--- e-com/backend/routes/productRoutes.js
+++ e-com/backend/routes/productRoutes.js
@@ -7,11 +7,15 @@ const router = express.Router();
 
 router.get("/", (req,res) => {
     res.json(products);
-})
+});
 
-router.get("/:id", (req,res) => {
+router.get(":id", (req,res) => {
     const product = products.find((p) => p._id === req.params.id);
     res.json(product);
+});
+
+router.get("/test", (req,res) => {
+    res.send("test");
 })
 
 export default router;
