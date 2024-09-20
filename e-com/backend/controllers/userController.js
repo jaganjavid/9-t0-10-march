d5f56161823efe7e061df93fb6d312dd5dae845f
jaganjavid <jagantutor1@gmail.com>
2024-09-20 22:50:52 +0530

    auth

--- e-com/backend/controllers/userController.js
+++ e-com/backend/controllers/userController.js
@@ -59,7 +59,7 @@ const registerUser = asyncHandler(async(req, res) => {
     })
 
     if(user){
-        console.log(user);
+        // console.log(user);
         generateToken(res, user._id);
 
         res.status(201).json({
--- e-com/backend/middleware/authMiddler.js
+++ e-com/backend/middleware/authMiddler.js
@@ -1,2 +1,47 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "./asyncHandler.js";
+import User from "../model/userModel.js";
+
 // protect
+
+
+const protect = asyncHandler(async (req, res, next) => {
+
+    let token = req.cookies.jwt;
+
+    console.log(token);
+
+    if(!token){
+        res.status(401);
+        throw new Error("Not authorized, no token");
+    }
+
+    try{
+        const decoded = jwt.verify(token, "secret");
+        // console.log(decoded);
+        req.user = await User.findById(decoded.userId);
+        console.log(req.user);
+        next();
+    }catch(err){
+        res.status(401);
+        throw new Error("Not authorized,token failed");
+    }
+
+
+
+})
+
+
+
 // admin
+
+const admin = (req, res, next) => {
+    if(req.user && req.user.isAdmin){
+        next();
+    }else{
+        res.status(401);
+        throw new Error("Not auth ,as admin");
+    }
+}
+
+export {protect, admin};
